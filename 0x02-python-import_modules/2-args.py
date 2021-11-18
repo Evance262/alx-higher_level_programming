c1be84686ca6d7caec6a320d78cf381be9c4fbb9
Evans262 <evancechapuma62@gmail.com>
2021-11-18 15:44:45 +0200

    making scripts dynamic

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
+    """Prints the number of and the list of its arguments."""
     import sys
 
     num_arg = len(sys.argv) - 1   # number of arguments parsed
