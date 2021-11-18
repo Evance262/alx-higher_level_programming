1862d1e26fba6ae63fad954ff453b27febc3e5a4
Evans262 <evancechapuma62@gmail.com>
2021-11-18 14:29:45 +0200

    simple import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-from add_0 import add
-sum = add(1,2)
-print("1 + 2 = {}".format(sum))
+
+if __name__ == "__main__":
+    """Print some of 1 and 2"""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
