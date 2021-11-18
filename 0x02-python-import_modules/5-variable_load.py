4720d91b667db1825964b4975bef348af60da1e5
Evans262 <evancechapuma62@gmail.com>
2021-11-18 19:01:07 +0200

    bug fix

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     """Prints the value of a from the variable_oad_5 file."""
     from  variable_load_5 import a
     
-    print(a)
+    print("{}".format(a))
