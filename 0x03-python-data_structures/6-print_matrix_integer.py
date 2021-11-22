5002f75de4dfc034ee00dbbad2f4e8bbf68eb1bc
Evans262 <evancechapuma62@gmail.com>
2021-11-22 16:08:23 +0200

    bug test

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,10 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for line in matrix:
-        print(' '.join(map(str, line)))
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end="")
+            if j != (len(matrix[i]) - 1):
+                print(" ", end="")
+
+        print("")
