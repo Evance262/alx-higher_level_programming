f275817099bca586f3dc97f430283b7cfb7989d8
Evans262 <evancechapuma62@gmail.com>
2021-11-25 00:53:36 +0200

    bug fix

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -2,12 +2,7 @@
 
 
 def uniq_add(my_list=[]):
-    uniq_number_list = []
-
-    uniq_numbers = set(my_list)
-
-    for number in uniq_numbers:
-        uniq_number_list.append(number)
-        addition_uniq = sum(uniq_number_list)
-
-    return (addition_uniq)
+    summation = 0
+    for i in set(my_list):
+        summation += i
+    return (summation)
