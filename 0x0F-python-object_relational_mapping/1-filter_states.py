429f0d1c157faed66eaab004a8e05527d6c4b716
Evans262 <evancechapuma62@gmail.com>
2022-03-09 21:29:45 +0200

    Filter sates

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,7 @@ def main(argv):
     conn = MySQLdb.connect(host="localhost", port="3306",
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT + FROM states WHERE name >= N")
+    cur.execute("SELECT * FROM states WHERE name >= N")
     rows = cur.fetchall()
     for row in rows:
         print(row)
