46806f007f3c733f5538e6c1361b3ad8abc5df02
Evans262 <evancechapuma62@gmail.com>
2022-03-09 20:15:17 +0200

    test run

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-
+"""
+takes 3 arguments: mysql username, mysql password and database name
+"""
+import sys
 import MySQLdb
 
-conn = MySQLdb.connect(host="localhost", port=3306,\
-user="evanz", passwd="signin", db="hbtn_0e_0_usa")
 
-cur  = db.cursor()
+def main(argv):
+    """Connects and lists all states from the database hbtn_0e_0_usa"""
+    conn = MySQLdb.connect(host="localhost", port=3306,
+    user=argv[1], passwd=argv[2], db=argv[3])
 
-cur.execute("SELECT * FROM states WHERE id = 1")
+    cur  = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
 
-rows = cur.fetchall()
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    conn.close()
 
-for row in rows:
-    print("%s" % col)
 
-print("\n")
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        main(sys.argv)
