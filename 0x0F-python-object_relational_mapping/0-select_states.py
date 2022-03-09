a76184790276deafb9619a204209210856695125
Evans262 <evancechapuma62@gmail.com>
2022-03-09 19:16:30 +0200

    trial run

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import MySQLdb
+
+conn = MySQLdb.connect(host="localhost", port=3306,\
+user="evanz", passwd="signin", db="hbtn_0e_0_usa")
+
+cur  = db.cursor()
+
+cur.execute("SELECT * FROM states WHERE id = 1")
+
+print("Id: %s -- Title: %s" % cur.fetchone())
