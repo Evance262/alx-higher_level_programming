c66deb3576cfab55c96e1d47063d03f79cc4537f
Evans262 <evancechapuma62@gmail.com>
2022-03-09 21:26:41 +0200

    Filter sates

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Lits states with a name starting with N (upper N)
+from the database hbtn_0e_usa
+"""
+import sys
+import MySQLdb
+
+
+def main(argv):
+    """Connects to a given mysql database"""
+    conn = MySQLdb.connect(host="localhost", port="3306",
+                           user=argv[1], passwd=argv[2], db=argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT + FROM states WHERE name >= N")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+        # cleans up the cursor
+        cur.close()
+        # closes the database connection
+        conn.close()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 4:
+        main(sys.argv)
