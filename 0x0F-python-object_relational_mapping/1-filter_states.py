2433124520d432cb771599cd34f45276153a8436
Evans262 <evancechapuma62@gmail.com>
2022-03-09 21:58:28 +0200

    Filter sates

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,7 +12,8 @@ def main(argv):
     conn = MySQLdb.connect(host="localhost", port="3306",
                            user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name >= N")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%'\
+                ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
