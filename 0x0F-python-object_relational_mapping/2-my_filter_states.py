b5190696472efaf53e48d020b2c4387144dec8b0
Evans262 <evancechapuma62@gmail.com>
2022-03-09 22:21:47 +0200

    Filter states by user

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -10,7 +10,7 @@ import MySQLdb
 def main(argv):
     """Connects to a given mysql database"""
     conn = MySQLdb.connect(host="localhost", port=3306,
-                           user=argv[1], passwd=argv[2], db=argv[3], state=argv[4])
+                           user=argv[1], passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}%'\
                 ORDER BY id ASC".format(argv[4]))
