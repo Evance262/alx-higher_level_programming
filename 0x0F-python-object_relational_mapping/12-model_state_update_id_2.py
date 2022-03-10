8f449602a8509df97b3b657d61cc0b265c6fdaa5
Evans262 <evancechapuma62@gmail.com>
2022-03-11 00:06:19 +0200

    updates state id

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import sessionmaker
 
 def main(argv):
     """ main function """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}/port=3306'.format(
                             argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
