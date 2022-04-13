da6aea7935523f4c0e0ca17a9ef1508e26eeea4d
Evans262 <evancechapuma62@gmail.com>
2022-04-13 22:11:08 +0200

    Write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8',
-  function (error) {
-  /* If an error exists, show it */
-    if (error) console.log(error);
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', (err, data) => {
+  if (err) console.log(err);
 });
